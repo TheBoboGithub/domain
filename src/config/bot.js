82bbd12c574dcd6af2b6e4dce1635cbe9191574c
TheBoboGithub <160724930+TheBoboGithub@users.noreply.github.com>
2026-04-25 00:08:04 +0100

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -88,8 +88,8 @@ export const botConfig = {
   embeds: {
     colors: {
       // Main brand colors.
-      primary: "#336699", 
-      secondary: "#2F3136", 
+      primary: "#000000", 
+      secondary: "#A9A9A9", 
 
       // Standard status colors for success/error/warning/info messages.
       success: "#57F287", 
